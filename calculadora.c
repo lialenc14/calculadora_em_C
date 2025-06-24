8d98063b4ee1f230734d98e3e48536d19093a35e
Livia Alencar <109006719+lialenc14@users.noreply.github.com>
2025-06-24 19:52:10 -0300

    Update calculadora.c
    adicionando feature de saída de dados do resultado da operação

--- calculadora.c
+++ calculadora.c
@@ -1,15 +1,18 @@
-#include <stdio.h>
-int main() {
-    printf("Calculadora\n");
+#include<stdio.h>
+#include<locale.h>
+#include<stdlib.h>
+
+void main(){
+    setlocale(LC_ALL, "pt_BR.UTF-8");
     int opcao;
+    float num1, num2;
     printf("Digite o número correspondente à operação desejada: \n");
     printf("1 - somar, 2 - subtrair, 3 - multiplicar, 4 - dividir: \n");
     scanf("%d", &opcao);
+    
     printf("Digite o primeiro número da operação: \n");
-    float num1;
     scanf("%f", &num1);
     printf("Digite o segundo número da operação: \n");
-    float num2;
     scanf("%f", &num2);
 
     float somresult = num1 + num2;
@@ -19,6 +22,19 @@ int main() {
 
     switch (opcao) {
         case 1:
-        printf("O resultado da operação é:");
+            printf("O resultado da operação é: %.2f\n", somresult);
+            break;
+        case 2:
+            printf("O resultado da operação é: %.2f\n", subresult);
+            break;
+        case 3:
+            printf("O resultado da operação é: %.2f\n", multresult);
+            break;
+        case 4:
+            printf("O resultado da operação é: %.2f\n", divresult);
+            break;
+        default:
+            printf("Operação inexistente\n");
+            break;
     }
 }
